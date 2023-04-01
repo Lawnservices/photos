0cbf17a58c63ec7246fcb884ab70ea5e2ea6ade0
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-04-01 13:39:54 -0400

    fix-web

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 document.querySelector('#demo').innerHTML = ' Good Services Call Now (240)883-8479';
+
+const masa = document.getElementById('header','blanco');
+ 
+
+document.addEventListener('scroll', () => {
+  var scroll_position = window.scrollY;
+  if(scroll_position > 160) {
+    header.style.height = '80px' ;
+    header.style.backgroundColor = '#299684' ;
+    header.style.textAlign = 'left' ;
+     
+     
+  } else{
+    header.style.height = '120px';
+    header.style.backgroundColor = '#f6f6f6' ;
+     header.style.textAlign = 'center' ;
+      
+  }
+})
