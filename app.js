9e10acbfb7a84c3f4070a35750819b808e63ca1f
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-11-10 19:36:42 -0500

    good
    today fix web

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', function(){
 document.querySelector('#demo').innerHTML = ' Good Services Call Now (240)883-8479';
 
 const masa = document.getElementById('header','blanco');
@@ -5,16 +6,19 @@ const masa = document.getElementById('header','blanco');
 
 document.addEventListener('scroll', () => {
   var scroll_position = window.scrollY;
-  if(scroll_position > 160) {
-    header.style.height = '180px' ;
-    header.style.backgroundColor = '#299684' ;
-    header.style.textAlign = 'left' ;
-     
+  if(scroll_position > 60) {
+    masa.style.height = '100px' ;
+    masa.style.backgroundColor = '#299684' ;
+    masa.style.textAlign = 'left' ;
+    demo.style.display = 'block'; 
+    demo.style.fontSize= '18px';
      
   } else{
-    header.style.height = '120px';
-    header.style.backgroundColor = '#f6f6f6' ;
-     header.style.textAlign = 'center' ;
+    masa.style.height = '100px';
+    masa.style.backgroundColor = '#f6f6f6' ;
+    masa.style.textAlign = 'center' ;
+    demo.style.display = 'none'; 
       
   }
 })
+});
