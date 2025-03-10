07e50b5357f99678893b10e4792042154314fe04
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-03-10 15:42:49 -0400

    fix-web

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-document.querySelector('#demo').innerHTML = ' Good Services Call Now (240)883-8479';
+ 
 
 const masa = document.getElementById('header','blanco');
  
@@ -7,17 +7,19 @@ const masa = document.getElementById('header','blanco');
 document.addEventListener('scroll', () => {
   var scroll_position = window.scrollY;
   if(scroll_position > 60) {
-    masa.style.height = '100px' ;
+    masa.style.height = '60px' ;
     masa.style.backgroundColor = '#299684' ;
     masa.style.textAlign = 'left' ;
     demo.style.display = 'block'; 
     demo.style.fontSize= '18px';
+    masa.style.padding = '20px';
      
   } else{
     masa.style.height = '100px';
     masa.style.backgroundColor = '#f6f6f6' ;
     masa.style.textAlign = 'center' ;
     demo.style.display = 'none'; 
+    
       
   }
 })
