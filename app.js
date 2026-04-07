77b856549723e739815bb02f3491ea4984829797
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-04-07 10:19:24 -0400

    fix-web

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-document.addEventListener('DOMContentLoaded', function(){
- 
+document.addEventListener('DOMContentLoaded', function () {
 
-const masa = document.getElementById('header','blanco');
- 
 
-document.addEventListener('scroll', () => {
-  var scroll_position = window.scrollY;
-  if(scroll_position > 60) {
-    masa.style.height = '60px' ;
-    masa.style.backgroundColor = '#299684' ;
-    masa.style.textAlign = 'left' ;
-    demo.style.display = 'block'; 
-    demo.style.fontSize= '18px';
-    masa.style.padding = '20px';
-     
-  } else{
-    masa.style.height = '100px';
-    masa.style.backgroundColor = '#f6f6f6' ;
-    masa.style.textAlign = 'center' ;
-    demo.style.display = 'none'; 
-    
-      
-  }
-})
+  const masa = document.getElementById('header', 'blanco');
+
+
+  document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 160) {
+      masa.style.height = '80px';
+      masa.style.backgroundColor = '#061125';
+      masa.style.textAlign = 'left';
+      demo.style.display = 'block';
+      demo.style.fontSize = '16px';
+
+
+    } else {
+      masa.style.height = '80px';
+      masa.style.backgroundColor = '#f6f6f6';
+      masa.style.textAlign = 'center';
+      demo.style.display = 'none';
+
+
+    }
+  });
+
+  let fecha =  new Date();
+  let fechas = fecha.getFullYear();
+
+  document.getElementById('fecha').textContent = `© ${fechas} Laguna Lawn Services. All Rights Reserved.`;
 });
